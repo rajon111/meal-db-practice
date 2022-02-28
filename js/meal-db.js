d9f7366f33b5985f9fb1d5e3c4cb4756eb584e92
rajon <mostofarajon111@gmail.com>
2022-02-28 18:43:13 +0600

    error handling added

--- js/meal-db.js
+++ js/meal-db.js
@@ -1,32 +1,66 @@
-const searchFood =()=>{
+const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
 
-    searchField.value =''
+    searchField.value = ''
+    if (searchText == '') {
+        alert('write something')
+    } else {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${ searchText }`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.meals))
 
-    const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displaySearchResult(data.meals))
+    }
+
 
 }
 
-const displaySearchResult= meals =>{
-    // console.log(meals)
-    const searchResult = document.getElementById('search-result')
-    meals.forEach(meal => {
-        // console.log(meal)
-        const div = document.createElement('div')
-        div.classList.add('col')
-        div.innerHTML=`
-        <div class="card">
-             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+const displaySearchResult = meals => {
+    
+    if (meals == null) {
+        alert('not found')
+    } else {
+        const searchResult = document.getElementById('search-result')
+        // searchResult.innerHTML = '';
+        searchResult.textContent = ''
+        meals.forEach(meal => {
+            // console.log(meal)
+            const div = document.createElement('div')
+            div.classList.add('col')
+            div.innerHTML = `
+        <div onclick="loadMealDetail(${ meal.idMeal })" class="card">
+             <img src="${ meal.strMealThumb }" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">${ meal.strMeal }</h5>
+                 <p class="card-text">${ meal.strInstructions.slice(0, 150) }</p>
             </div>
         </div>
         `;
-        searchResult.appendChild(div)
-    });
+            searchResult.appendChild(div)
+        });
+    }
+
+}
+
+const loadMealDetail = mealId => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ mealId }`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetail(data.meals[0]))
+}
+
+const displayMealDetail = meal => {
+    const mealDetails = document.getElementById('meal-details')
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+    <img src="${ meal.strMealThumb }" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${ meal.strMeal }</h5>
+      <p class="card-text">${ meal.strInstructions.slice(0, 150) }</p>
+      <a target="_blank" href="${ meal.strYoutube }" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    mealDetails.appendChild(div)
 }
